e7fc810e11da65a1511c610fc01a41c64c4c6bd9
Aagat Adhikari <hi@aagat.com>
2017-02-25 04:22:01 +0545

    added basic layout from example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+import './App.css';
 
 class App extends Component {
+  state = {
+      collapsed: true,
+      mode: 'inline'
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <p>Hello world!</p>
-      </div>
+      <Layout>
+        <Sider
+          collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={ this.toggle }
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 480 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
     );
   }
 }
