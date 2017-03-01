e71842ad63f6f6170eb697af6fc60c6c2241bc44
Aagat Adhikari <hi@aagat.com>
2017-03-01 20:47:55 +0545

    link side navigation to components

--- src/App.js
+++ src/App.js
@@ -22,11 +22,6 @@ const routes = [
         exact: true
     },
     {
-        path: "/overview",
-        component: Overview,
-        exact: true
-    },
-    {
         path: "/runtime",
         component: Runtime,
         exact: true
@@ -69,7 +64,7 @@ class App extends Component {
               <Content style={{ padding: '0 50px' }}>
                   <Layout style={{ padding: '24px 0', background: '#fff', margin: '24px 0' }}>
                       <Sider width={200} style={{ background: '#fff' }}>
-                          <SideMenu/>
+                          <SideMenu routes={routes}/>
                       </Sider>
                       <Content style={{ padding: '0 24px', minHeight: 280 }}>
                           {routes.map((route, i) => {
