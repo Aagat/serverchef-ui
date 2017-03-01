7c72071c57d3e806c19536487e47b0b6e675b8f7
Aagat Adhikari <hi@aagat.com>
2017-03-01 14:50:41 +0545

    seperate SideMenu into seperate component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Icon, Table, Button } from 'antd';
+import { Layout, Icon, Table, Button } from 'antd';
 import { Row, Col, Card, Badge } from 'antd';
-const { SubMenu } = Menu;
 const { Header, Sider, Content, Footer } = Layout;
 
-import './App.css'; import TopMenu from './components/ui/TopMenu';
+import './App.css';
+import TopMenu from './components/ui/TopMenu';
+import SideMenu from './components/ui/SideMenu';
 
 class App extends Component {
 
@@ -150,39 +151,7 @@ class App extends Component {
               <Content style={{ padding: '0 50px' }}>
                   <Layout style={{ padding: '24px 0', background: '#fff', margin: '24px 0' }}>
                       <Sider width={200} style={{ background: '#fff' }}>
-                          <Menu
-                              mode='inline'
-                              defaultSelectedKeys={['1']}
-                              style={{ height: '100%' }} >
-                              <Menu.Item key='1'>
-                                  <Icon type="line-chart" />
-                                  <span>Overview</span>
-                              </Menu.Item>
-                              <Menu.Item key='2'>
-                                  <Icon type="code-o" />
-                                  <span>Runtime</span>
-                              </Menu.Item>
-                              <Menu.Item key='3'>
-                                  <Icon type="hdd" />
-                                  <span>Database</span>
-                              </Menu.Item>
-                              <Menu.Item key='4'>
-                                  <Icon type="select" />
-                                  <span>Utilities</span>
-                              </Menu.Item>
-                              <Menu.Item key='5'>
-                                  <Icon type="link" />
-                                  <span>Network</span>
-                              </Menu.Item>
-                              <Menu.Item key='7'>
-                                  <Icon type="file-text" />
-                                  <span>Logs</span>
-                              </Menu.Item>
-                              <Menu.Item key='8'>
-                                  <Icon type="setting" />
-                                  <span>Settings</span>
-                              </Menu.Item>
-                          </Menu>
+                          <SideMenu/>
                       </Sider>
                       <Content style={{ padding: '0 24px', minHeight: 280 }}>
                           <h1>Overview</h1>
